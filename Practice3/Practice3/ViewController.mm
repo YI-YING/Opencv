c56a6c09b72cf67f490ebaa7953c666b196d5cda
YI-YING <01360912@me.mcu.edu.tw>
2017-03-30 22:01:44 +0800

    make some directory change

--- Practice3/Practice3/ViewController.mm
+++ Practice3/Practice3/ViewController.mm
@@ -12,13 +12,13 @@ using namespace std;
 using namespace cv;
 
 typedef struct {
-    unsigned char Blue[8];
-    unsigned char Green[8];
-    unsigned char Red[8];
+    unsigned char Blue;
+    unsigned char Green;
+    unsigned char Red;
 } Palette_COLOR_TYPE;
 
 const int iColorNum = 256;
-const int iWidth = 400, iHeight = 736, iCellWidth = 50, iCellHeight = 23;
+const int iWidth = 400, iHeight = 704, iCellWidth = 50, iCellHeight = 22;
 const int iCellRows = iHeight / iCellHeight, iCellCols = iWidth / iCellWidth;
 Palette_COLOR_TYPE palette;
 
@@ -33,9 +33,13 @@ Palette_COLOR_TYPE palette;
     [super viewDidLoad];
     
     Mat matCanvas = Mat(Size_<int>(iWidth, iHeight), CV_8UC3, Scalar(255, 255, 255));
+    [self makeColorSet];
+    [self plotPalette:matCanvas];
     
+    UIImageView *uiimageview = [[UIImageView alloc] initWithImage:[UIImage UIImageFromCVMat:matCanvas]];
+    uiimageview.frame = CGRectMake(0, 20, iWidth, iHeight);
     
-    
+    [self.view addSubview:uiimageview];
 }
 
 - (void)makeColorSet
