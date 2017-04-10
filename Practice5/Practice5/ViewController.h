9679eaff9e0e1d32729aead18d9b2777d27bd16b
YI-YING <a3852870@gmail.com>
2017-04-10 09:46:03 +0800

    add Practice 5

---
+++ Practice5/Practice5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Practice5
+//
+//  Created by MCUCSIE on 4/9/17.
+//  Copyright © 2017 MCUCSIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Practice5/Practice5/ViewController.mm
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Practice5
+//
+//  Created by MCUCSIE on 4/9/17.
+//  Copyright © 2017 MCUCSIE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+}
+
+@end
